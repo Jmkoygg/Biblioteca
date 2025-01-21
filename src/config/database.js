006a674ea25343a1427412274a324f44f9c212a2
João Soares <jmkoygg@gmail.com>
2025-01-21 15:48:28 -0300

    implement USER repositories

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('library_bd.sqlite3', (err) => {
+    if (err) { 
+        console.error('Erro ao conectar ao banco de dados', err.message);
+    } else { 
+        console.log('Conectado ao banco de dados com sucesso');
+    }
+})
+
+export default db;
---
+++ src/repositories/usersRepositorie.js
@@ -0,0 +1,27 @@
+import db from "../config/database.js";
+db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT,
+        avatar TEXT
+    )
+    `)
+
+    function createUserRepository(newUser){7
+        return new Promise((resolve, reject) => {
+            const { name, email, password, avatar } = newUser;
+            db.run(`
+                INSERT INTO users (name, email, password, avatar) 
+                VALUES (?, ?, ?, ?)
+                `, [name, email, password, avatar], 
+                (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({message: "Usuário criado com sucesso"});
+                    }
+                })
+            })
+        }
